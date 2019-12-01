f94a2eb80b9993b15f892f09f41142b8b454dc42
Yudi Yamane <yudiyamane@gmail.com>
2019-11-30 23:24:08 -0300

    commit to deploy

--- matrix.js
+++ matrix.js
@@ -5,7 +5,6 @@ let SYMBOL_WIDTH
 
 if (navigator.userAgent.indexOf('IEMobile') !== 1) {
   SYMBOL_WIDTH = 60
-  document.getElementsByTagName('body')[0].style.background='white'
 }
 else {
   SYMBOL_WIDTH = 24
